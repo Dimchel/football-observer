509fa41ee690f02c8d96ce4595fe87b859e93d98
d.kidakov <d.kidakov@indriver.com>
2024-02-19 14:58:54 +0200

    leagues screen facelift

--- feature/leagues/src/main/kotlin/com/dimchel/fa/feature/leagues/ui/LeaguesScreen.kt
+++ feature/leagues/src/main/kotlin/com/dimchel/fa/feature/leagues/ui/LeaguesScreen.kt
@@ -1,7 +1,9 @@
 package com.dimchel.fa.feature.leagues.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +12,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,7 +35,7 @@ internal fun LeaguesScreen(
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
-    FaTheme {
+    FaTheme(dynamicColor = false) {
         Surface {
             when (val state = uiState) {
                 is LeaguesUiState.Loading -> LoadingState()
@@ -53,7 +57,7 @@ internal fun LoadingState() {
 
 @Composable
 internal fun ErrorState() {
-    // TODO: Implement this
+    Text(text = "Some error happened")
 }
 
 @Composable
@@ -73,23 +77,38 @@ internal fun LeagueCard(
     league: LeagueModel,
     onLeagueClicked: (leagueId: Int) -> Unit,
 ) {
-    Row(
-        modifier = Modifier.clickable { onLeagueClicked(league.id) },
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        AsyncImage(
-            modifier = Modifier
-                .padding(16.dp)
-                .size(32.dp),
-            model = league.emblemUrl,
-            contentDescription = null,
-        )
-        Spacer(modifier = Modifier.size(8.dp))
-        Text(
-            modifier = Modifier
-                .fillMaxSize(),
-            text = league.name,
-        )
-        HorizontalDivider()
+    Column {
+        Row(
+            modifier = Modifier.clickable { onLeagueClicked(league.id) },
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(
+                        start = 16.dp,
+                        top = 4.dp,
+                        bottom = 4.dp,
+                    )
+                    .background(
+                        color = MaterialTheme.colorScheme.secondaryContainer,
+                        shape = RoundedCornerShape(12.dp)
+                    ),
+                contentAlignment = Alignment.Center,
+            ) {
+                AsyncImage(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .size(48.dp),
+                    model = league.emblemUrl,
+                    contentDescription = null,
+                )
+            }
+            Spacer(modifier = Modifier.size(16.dp))
+            Text(
+                modifier = Modifier.fillMaxSize(),
+                text = league.name,
+            )
+        }
+        HorizontalDivider(color = MaterialTheme.colorScheme.secondaryContainer)
     }
 }
